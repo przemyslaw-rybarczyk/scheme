4fe6531cd00b7cb4e09118a17f647e623c26ea14
Przemysław Rybarczyk <przemyslaw.rybarczyk@protonmail.com>
2019-09-19 01:03:57 +0200

    Made parser treat control codes like whitespace

--- unicode/unicode.h
+++ unicode/unicode.h
@@ -5,6 +5,7 @@ int is_uppercase(char32_t c);
 int is_alphabetic(char32_t c);
 int is_numeric(char32_t c);
 int is_whitespace(char32_t c);
+int is_control(char32_t c);
 char32_t to_uppercase(char32_t c);
 char32_t to_lowercase(char32_t c);
 char32_t fold_case(char32_t c);
