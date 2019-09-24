11414a468bebc4d98df24d93f4d1b7395c5e0c3c
Przemysław Rybarczyk <przemyslaw.rybarczyk@protonmail.com>
2019-09-24 16:35:39 +0200

    Modified compile.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-"${CC:-gcc}" -O3 -Wall -Wconversion *.c primitives/*.c -o scheme "${@:1}" &&
-cat compiler.scm macro.scm > _compiler.scm &&
-./scheme _compiler.scm --compile compiler.sss &&
-./scheme _compiler.scm --compile _compiler.sss &&
+set -e
+
+"${CC:-gcc}" -O3 -Wall -Wconversion *.c primitives/*.c -o scheme "${@:1}"
+cat compiler.scm macro.scm > _compiler.scm
+./scheme _compiler.scm --compile compiler.sss
+./scheme _compiler.scm --compile _compiler.sss
 diff compiler.sss _compiler.sss || echo "Warning: compiler does not reproduce itself"
 rm _compiler.scm
 rm _compiler.sss
