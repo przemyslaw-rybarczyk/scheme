27d0cc9778cd6d598ce7604be4d6580dee3ac49e
Przemysław Rybarczyk <przemyslaw.rybarczyk@protonmail.com>
2019-09-30 14:24:56 +0200

    Added ability to execute multiple files

--- compile.sh
+++ compile.sh
@@ -3,9 +3,7 @@
 set -e
 
 "${CC:-clang}" -O3 -Wall -Wconversion -Wstrict-prototypes *.c primitives/*.c unicode/unicode.c -o scheme "${@:1}"
-cat compiler.scm macro.scm > _compiler.scm
-./scheme _compiler.scm --compile compiler.sss
-./scheme _compiler.scm --compile _compiler.sss
+./scheme compiler.scm macro.scm --compile compiler.sss
+./scheme compiler.scm macro.scm --compile _compiler.sss
 diff compiler.sss _compiler.sss || echo "Warning: compiler does not reproduce itself"
-rm _compiler.scm
 rm _compiler.sss
