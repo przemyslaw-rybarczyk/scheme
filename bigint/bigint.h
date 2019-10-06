feb8ea2f401dc0d8cb9ebe21e3a8e8e8c288be6a
Przemysław Rybarczyk <przemyslaw.rybarczyk@protonmail.com>
2019-10-06 02:32:20 +0200

    Added double_base typedef

--- bigint/bigint.h
+++ bigint/bigint.h
@@ -1,6 +1,12 @@
 #include <inttypes.h>
 
+#ifdef __SIZEOF_INT128__
 typedef uint64_t base;
+typedef __uint128_t double_base;
+#else
+typedef uint32_t base;
+typedef uint64_t double_base;
+#endif
 
 typedef struct Bigint {
     size_t len;
